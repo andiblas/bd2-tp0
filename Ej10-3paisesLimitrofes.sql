cb3525f1eb94b70f24f735c3341ed683cd92d24e
gfgrillo3 <42618027+gfgrillo3@users.noreply.github.com>
2018-08-29 17:27:25 -0300

    Query triple frontera agregada

--- Ej10-3paisesLimitrofes.sql
+++ Ej10-3paisesLimitrofes.sql
@@ -17,3 +17,25 @@ inner join paises as p5 on p5.idPais = fron.pais5
 inner join paises as p6 on p6.idPais = fron.pais6
 
 ;
+
+
+
+--query triple frontera con producto cartesiano
+
+
+select p1.nombre, p2.nombre, p3.nombre from paises p1, paises p2, paises p3 
+	WHERE EXISTS
+				(select * from fronteras f1, fronteras f2 where 
+	   				((f1.idPais1 = p1.idPais and f1.idPais2 = p2.idPais)
+						or (f1.idPais2 = p1.idPais and f1.idPais1 = p2.idPais)) 
+	  				and ((f2.idPais1 = p1.idPais and f2.idPais2 = p3.idPais) 
+						or (f2.idPais2 = p1.idPais and f2.idPais1 = p3.idPais)))
+	and EXISTS
+				(select * from fronteras f3, fronteras f4 where 
+	   				((f3.idPais1 = p1.idPais and f3.idPais2 = p2.idPais) 
+							or (f3.idPais2 = p1.idPais and f3.idPais1 = p2.idPais)) 
+	  				and ((f4.idPais1 = p2.idPais and f4.idPais2 = p3.idPais) 
+							or (f4.idPais2 = p2.idPais and f4.idPais1 = p3.idPais)))
+	and p1.idPais < p2.idPais
+	and p2.idPais < p3.idPais																 
+;
