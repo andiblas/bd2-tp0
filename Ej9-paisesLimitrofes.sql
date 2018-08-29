75c784339590f421fe98d0e7958e69250bbba97f
andiblas <andy_blas@hotmail.com>
2018-08-29 12:04:01 -0300

    nada relevante, solo formateo

--- Ej9-paisesLimitrofes.sql
+++ Ej9-paisesLimitrofes.sql
@@ -1,8 +1,14 @@
---query paises limetes ordenado por extension
+-- query paises limites ordenado por extension
 
-select p1.nombre as nombre_pais1, p2.nombre as nombre_pais2, f.extension_km from fronteras as f
-inner join paises as p1
-on f.idpais1 = p1.idpais
-inner join paises as p2
-on f.idpais2 = p2.idpais
-order by f.extension_km desc
+select
+	p1.nombre as nombre_pais1,
+	p2.nombre as nombre_pais2,
+	f.extension_km
+from
+	fronteras as f
+inner join paises as p1 on
+	f.idpais1 = p1.idpais
+inner join paises as p2 on
+	f.idpais2 = p2.idpais
+order by
+	f.extension_km desc
